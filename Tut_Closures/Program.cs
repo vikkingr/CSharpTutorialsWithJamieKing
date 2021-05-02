b8e8c6fee516c98c29ff4f0346ba5acc435671aa
Robert Veinberg-Vikking <62076123+vikkingr@users.noreply.github.com>
2021-05-02 13:29:12 -0400

    Add files via upload

--- Tut_Closures/Program.cs
+++ Tut_Closures/Program.cs
@@ -1,1 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+	static void Main(string[] args)
+	{
+		
+		Action a = GiveMeAction();
+		a(); 
+		a(); 
+		a();
+		
+	}// main
+
+	static Action GiveMeAction() {
+		
+		Action ret = null;
+		
+		// Lambda Function! Looks like an arrow function.
+		int i = 0;
+		
+		ret += () => { 
+		
+			Console.WriteLine("First Method " + i++);
+		
+		};
+		ret += () => {
+
+			Console.WriteLine("Second Method " + i++);
+
+		};
+
+		// Return the chain
+		return ret;
+
+	}// method
+
+}// class
