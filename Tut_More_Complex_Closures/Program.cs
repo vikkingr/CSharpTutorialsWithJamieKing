a4b699c9281561e013c96a30f85a5d8c414f5682
Robert Veinberg-Vikking <62076123+vikkingr@users.noreply.github.com>
2021-05-02 13:39:06 -0400

    Add files via upload

--- Tut_More_Complex_Closures/Program.cs
+++ Tut_More_Complex_Closures/Program.cs
@@ -1,1 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello World!");
+
+	}// main
+
+	class Displayaodi {
+	
+		int i = 5;
+
+		public void myMethod1() {
+			i++;
+		}// method
+		public void myMethod2() { 
+			i+= 2;
+		}// method
+			
+	}// class
+
+	static Action GiveMeAction() { 
+	
+		Action ret = null;
+
+		var temp = new Displayaodi();
+		int j = 0;
+		int k = 0;
+		ret += temp.myMethod1;
+		ret += temp.myMethod2;
+
+		// Use a lambda expression to increment
+		ret += () => { j++; k++; };
+
+		return ret;
+	
+	}// method
+
+}// class
