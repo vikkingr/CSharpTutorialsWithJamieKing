5c3cab46c78c865999394bc5ba4afeb212bd25ad
Robert Veinberg-Vikking <62076123+vikkingr@users.noreply.github.com>
2021-05-02 13:36:22 -0400

    Add files via upload

--- Tut_Events_Add_And_Remove/Program.cs
+++ Tut_Events_Add_And_Remove/Program.cs
@@ -1,1 +1,54 @@
+﻿using System;
+using System.Diagnostics;
 
+namespace Tut_Compiler_s_Implementation_of_Events
+{
+	class Cow
+	{
+
+		private Action mooing;
+
+		// The event keyword adds "add" and "remove" methods
+		public event Action Mooing {
+
+			add
+			{
+				// This is done behind the scenes
+				mooing += value;
+			}
+			remove
+			{
+				mooing -= value;
+			}
+
+		}// event
+
+		public void PushSleepingCow()
+		{
+
+			// Ensure there are subscribers
+			if (mooing != null) {
+				mooing();
+			}// if
+
+		}// method
+
+	}// Cow
+
+	class Program
+	{
+
+		public static void Main(string[] args)
+		{
+
+			Cow c = new Cow();
+
+			c.Mooing += () => Console.WriteLine("The cow moved!");
+
+			c.PushSleepingCow();
+
+		}// main
+
+	}// class
+
+}// namespace
