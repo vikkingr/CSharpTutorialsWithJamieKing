cddf13d0b9520458e992c8cbad7cba23ad6ebfd2
Robert Veinberg-Vikking <62076123+vikkingr@users.noreply.github.com>
2021-05-02 13:30:05 -0400

    Add files via upload

--- Tut_Compiler_And_Closures/Program.cs
+++ Tut_Compiler_And_Closures/Program.cs
@@ -1,1 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+
+	class DisplayClass {
+	
+		public int i;
+		public void theMethodGeneratedFromTheLambdaExpression() {
+		
+			i++;
+		
+		}
+	
+	}// class
+
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello World!");
+
+		Action a = GiveMeAction();
+		a();
+		Action b = GiveMeAction();
+		b();
+		a();
+		a();
+
+	}// main
+
+	static Action GiveMeAction()
+	{
+		return new Action(new DisplayClass().theMethodGeneratedFromTheLambdaExpression);
+	}
+
+}// class
