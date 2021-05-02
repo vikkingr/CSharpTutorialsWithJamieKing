6c0fe65de57550e9353f3a4e913d343531cb6ac0
Robert Veinberg-Vikking <62076123+vikkingr@users.noreply.github.com>
2021-05-02 13:26:48 -0400

    Add files via upload

--- Tut_AnonymousMethods_And_LambdaFunctions/Program.cs
+++ Tut_AnonymousMethods_And_LambdaFunctions/Program.cs
@@ -1,1 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+
+	static bool EquivalentLambdaExpression(int i) { 
+		return i > 5;
+	}// method
+
+	static void Main(string[] args)
+	{
+		// new generic function that takes an int and returns a bool. Evaluates a lambda expression returning boolean if i > 5
+		Func<int, bool> func = i => i > 5;
+
+		// Anonymous method
+		Func<int, bool> func2 = delegate(int i) { return i > 5; };
+
+		// Pass in params
+		Console.WriteLine(func(3));
+		Console.WriteLine(func(7));
+
+	}// main
+
+}// class
