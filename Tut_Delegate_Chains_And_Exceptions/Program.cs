a9f8b72e308eecaa3ee5a104cf30e784049572ed
Robert Veinberg-Vikking <62076123+vikkingr@users.noreply.github.com>
2021-05-02 13:32:19 -0400

    Add files via upload

--- Tut_Delegate_Chains_And_Exceptions/Program.cs
+++ Tut_Delegate_Chains_And_Exceptions/Program.cs
@@ -1,1 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
 
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello World!");
+
+		Action del = (Action)Moo + BeNaughty + Goo;
+		foreach (Action a in del.GetInvocationList()) {
+
+			try { 
+			
+				a();
+			
+			} catch {
+			
+				// An empty catch just swallows an exception		
+
+			}
+
+		}
+		
+	}// main
+
+	static void Moo() { Console.WriteLine("Moo()"); }
+	static void BeNaughty() { throw new Exception(); }
+	static void Goo() { Console.WriteLine("Goo()"); }
+
+}// class
