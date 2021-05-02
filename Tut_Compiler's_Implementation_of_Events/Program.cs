cad4bdedea5d7a90881af426dbe9718028f94995
Robert Veinberg-Vikking <62076123+vikkingr@users.noreply.github.com>
2021-05-02 13:30:47 -0400

    Add files via upload

--- Tut_Compiler's_Implementation_of_Events/Program.cs
+++ Tut_Compiler's_Implementation_of_Events/Program.cs
@@ -1,1 +1,25 @@
+﻿using System;
 
+namespace Tut_Compiler_s_Implementation_of_Events
+{
+	class Cow {
+
+		public event Action Mooing;
+	
+	}// Cow
+
+	class Program {
+
+		public static void main(string[] args) {
+		
+			Cow c = new Cow();
+			
+			// Notice it is now a lightning bolt in the pop up menu. It is now an event, can't directly call it!
+			// UNCOMMENT BELOW
+			//c.Mooing();
+
+		// main
+	
+	}// class
+
+}// namespace
